da18606ed01559668579374e644a5bfc7a40cd0a
David <103114460+sceph@users.noreply.github.com>
2022-06-08 18:35:40 +0200

    Deleting / Changing Files

--- Try_It_Yourself/func01.py
+++
@@ -1,6 +0,0 @@
-# "Main" Program
-def place_to_go(place, *destinations):
-    print("\nWhere to go?: " + str(place))
-
-    for destination in destinations:
-        print("There: " + destination)
--- Try_It_Yourself/func02.py
+++
@@ -1,3 +0,0 @@
-import func01
-
-func01.place_to_go('USA')
